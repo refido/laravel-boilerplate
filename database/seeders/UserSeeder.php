8e83e76888f90f595fe0af3160fdf6190923032c
revido <revidoberliano@gmail.com>
2024-02-18 16:24:06 +0700

    templating and auth with fortify

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'user@user.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        // dd(auth()->user());
+        return view('dashboard.home');
+    })->name('home');
 });
